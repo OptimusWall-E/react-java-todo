80ee8314dd774bcf12158fd3e509ad428870375f
OptimusWall-E <iemailedmichael@gmail.com>
2022-12-03 17:54:05 +0000

    Added state. Changed hard coded const todos to const [todos, setTodos] = useState to track and set state. Changed todos.push(todos) to setTodos(todos => [...todos, newTodo]) to add hard coded new todos to to do list.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState([
     {rowNumber: 1, rowDescription: 'Type up notes', rowAssigned: 'User One'},
-    {rowNumber: 2, rowDescription: 'Set reading list', rowAssigned: 'User Two'},
-    {rowNumber: 3, rowDescription: 'Send job application', rowAssigned: 'User One'}
+    {rowNumber: 2, rowDescription: 'Set reading list', rowAssigned: 'User One'},
+    {rowNumber: 3, rowDescription: 'Send job application', rowAssigned: 'User Two'},
+    {rowNumber: 4, rowDescription: 'Prepare for interview', rowAssigned: 'User Two'}
   ]
+  )
+
+  // onClick=addTodo is a pointer. 
+  // We don't add () or it will be called automatically, without being clicked
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New task',
+        rowAssigned: 'User Three'
+      };
+      setTodos(todos => [...todos, newTodo]);
+    }
+  }
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -17,6 +32,9 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new task
+          </button>
         </div>
       </div>
     </div>
